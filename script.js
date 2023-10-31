820455210c1d0c7693e8f0874f7578ec16be1a9e
elmar <elmarcera@gmail.com>
2023-10-31 13:46:06 +0800

    PROJECT #1: Guess My Number!

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+// And more about that later.
+// Now into this method,
+// we need to pass in a Selector.
+// And again, this Selector
+// is exactly the same Selector
+// that we would use in CSS.
+
+// select the element
+// document.querySelector('.message')
+
+// select the element and read
+// document.querySelector('.message').textContent)
+
+console.log(document.querySelector('.message').textContent);
