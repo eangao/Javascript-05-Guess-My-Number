6ce0781575b4405f6a94bce26d809239de2d5efd
elmar <elmarcera@gmail.com>
2023-10-31 14:45:35 +0800

    Implementing the Game Logic

--- script.js
+++ script.js
@@ -43,6 +43,13 @@
 // Handling Click Events
 //////////////////////////////////////////
 
+// random number from 1 to 20
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   // console.log(document.querySelector('.guess').value);
   const guess = Number(document.querySelector('.guess').value);
@@ -50,5 +57,25 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
