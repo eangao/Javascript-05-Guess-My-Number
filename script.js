3a15711ef35b46147fc9aecc38d35b9c00c26b8a
elmar <elmarcera@gmail.com>
2023-10-31 14:07:00 +0800

    Selecting and Manipulating Elements

--- script.js
+++ script.js
@@ -23,3 +23,18 @@
 // What's the DOM and DOM Manipulation
 //////////////////////////////////////////////
 // See lecture
+
+////////////////////////////////////////////////
+// Selecting and Manipulating Elements
+////////////////////////////////////////////////
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+// use value to get the value and set the value
+document.querySelector('.guess').value = 23;
+
+console.log(document.querySelector('.guess').value);
